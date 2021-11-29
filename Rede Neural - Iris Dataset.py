b7e05a97108bafe8bb2f1df1e6a8836decba27dd
Ruan Guerra <89950619+R-Guerra@users.noreply.github.com>
2021-11-29 16:09:08 -0300

    Update Rede Neural - Iris Dataset.py

--- Rede Neural - Iris Dataset.py
+++ Rede Neural - Iris Dataset.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # ## Rede Neural com Normalização de Dados - Iris Dataset
-
-# In[15]:
-
 
 import numpy as np
 from sklearn import datasets
@@ -36,39 +30,20 @@ y_test = y[sufInd[100:]]
 classifier = MLPClassifier(solver ='sgd', hidden_layer_sizes=(3), learning_rate_init = 0.01, activation='logistic', max_iter=1500, random_state=1)#learning_rate_init=0.01 equevale a 1 para 100.
 classifier.fit(X_train, y_train) #Fit - Vai fazer o treinamento.
 y_prediction = classifier.predict_proba(X_test)#Faz a predição e probabilidade.
-#print(y_prediction)
 y_aux = np.argmax(y_prediction, 1)
 accuracy_score(y_test, y_aux)
 #print(y_prediction) #Sai com 3 probabilidades em cada linha de dados
 
-
-# In[14]:
-
-
 y_prediction
 
-
-# In[16]:
-
-
 print(y_test[:10], y_aux[:10])
-
-
-# In[12]:
-
 
 X_train
 
 
-# ### Conclusões
+#Conclusões
 
 # - Antes da normalização, a rede neural obteve apenas 28% de accuracy. Após a normalização dos dados, obteve 86%.
 # - Com o learning_rate_init saindo de 0.001 para 0.01, se obteve uma accuracy de 98% (modificações mais radicais mantém o mesmo valor)
 # - A normalização foi feita entre os valores de -1 e 1.
 # - Taxa de variação dos valores na mesma faixa, com isso a rede neural pôde otimizar o aprendizado.
-
-# In[ ]:
-
-
-
-
